d6ed430bb3ef5eef47042aeb92dcac1357588b35
AlexanderVLRS <96099152+AlexanderVLRS@users.noreply.github.com>
2022-01-13 00:05:47 +1000

    feat: add rev

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function rev(n) {
+    
+    //Получение модуля числа
+    const mod = Math.abs(n);
+    //console.log('Mod: ', mod);
+    
+    //Переобразование в строку и разделение на массив
+    const numsArr = mod.toString().split('');
+    // console.log('numsArr: ', numsArr)   
+
+    //Переворот массива
+    const reversedArr = numsArr.reverse();
+     // console.log('reversArr: ', reversedArr);
+     
+    //Преобразование массива в строку. parseInt переводит строку обратно в число.
+    return parseInt(reversedArr.join(''));
+    };
+
+    //Для проверки конечного результата
+    // console.log(rev(-165));
